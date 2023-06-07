c0b7ae07ae8295c23da94306941cbcac0ebe2177
Minh-Ngoc <ngocminh101100@gmail.com>
2023-06-07 09:47:43 +0700

    complete UI PC

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -22,7 +22,7 @@ const Products = ({ title, data, filterIcon, filterText, slider, ...props }) => 
     const settings = {
         dots: true,
         infinite: true,
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
